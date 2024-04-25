da7f313cab0a68c8da44fde48ebaa901a3f7d7c6
pikloo <p.loukakou@gmail.com>
2024-04-26 01:37:11 +0200

    ''

--- website/wp-content/themes/encrage/partials/series.php
+++ website/wp-content/themes/encrage/partials/series.php
@@ -1,16 +1,63 @@
+<?php
+$series = [
+    [
+        'title' => 'Lorem Ipsum',
+        'author' => 'Leo kekemenis',
+        'year' => '2023'
+    ],
+    [
+        'title' => 'Lorem Ipsum',
+        'author' => 'John Doe',
+        'year' => '2023'
+    ],
+    [
+        'title' => 'Lorem Ipsum',
+        'author' => 'Jane Smith',
+        'year' => '2023'
+    ],
+    [
+        'title' => 'Lorem Ipsum',
+        'author' => 'John Doe',
+        'year' => '2023'
+    ],
+    [
+        'title' => 'Lorem Ipsum',
+        'author' => 'John Doe',
+        'year' => '2023'
+    ],
+    [
+        'title' => 'Lorem Ipsum',
+        'author' => 'John Doe',
+        'year' => '2023'
+    ],
+    [
+        'title' => 'Lorem Ipsum',
+        'author' => 'John Doe',
+        'year' => '2023'
+    ]
+]
+?>
+
 <section class="container mx-auto py-10">
     <h2>Séries</h2>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 px-4">
-        <figure>
-            <img src="./wp-content/themes/encrage/assets/images/serie1.png" alt="" />
-            <figcaption class="xl:hidden">
-                <h3>
-                    <span class="after:h-[1px] after:bg-black
-              after:inline-block after:relative after:w-[20px] after:align-middle after:ml-1">Lorem Ipsum</span>
-                    <span>Leo kekemenis</span>
-                </h3>
-                <span>2023</span>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
+        <?php for($i=1;$i<4;$i++) :?>
+        <figure class="relative group xl:overflow-hidden">
+            <img src="./wp-content/themes/encrage/assets/images/serie<?= $i ?>.png" alt="" />
+            <figcaption class="xl:hidden xl:absolute xl:group-hover:block xl:bottom-0 xl:text-white xl:group-hover:animate-slideInUp">
+                <div class="xl:bg-black xl:px-4">
+                    <h3 class="mt-4">
+                        <span class="after:h-[1px] after:bg-black xl:after:bg-white
+              after:inline-block after:relative after:w-[20px] after:align-middle after:ml-1"><?= $series[$i]['title'] ?></span>
+                        <span class="ml-2 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-black xl:before:bg-white relative inline-block">
+                            <span class="relative text-white xl:text-black"><?= $series[$i]['author'] ?></span>
+                        </span>
+                    </h3>
+                    <span class="text-slate-500"><?= $series[$i]['year'] ?></span>
+                </div>
             </figcaption>
         </figure>
+        <?php endfor;?>
     </div>
+    <button type="button">plus de séries</button>
 </section>
--- website/wp-content/themes/encrage/tailwind.config.js
+++ website/wp-content/themes/encrage/tailwind.config.js
@@ -7,6 +7,20 @@ module.exports = {
 			fontFamily: {
 				ibm: [ 'ArimoIBM Plex Sans', 'Arial', 'sans-serif' ],
 			},
+			keyframes: {
+				"slide-in-up": {
+					"0%": {
+						visibility: "visible",
+						transform: "translate3d(0, 100%, 0)",
+					},
+					"100%": {
+						transform: "translate3d(0, 0, 0)",
+					},
+				},
+			},
+			animation: {
+                slideInUp: "slide-in-up 0.5s ease-in-out",
+            },
 		},
 	},
 	plugins: [],
