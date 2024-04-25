98808c5c872acd8aff020a090b3884d939df4895
pikloo <p.loukakou@gmail.com>
2024-04-25 14:49:49 +0200

    'chargement tailwind, index.js et google font'

--- website/wp-content/themes/encrage/functions.php
+++ website/wp-content/themes/encrage/functions.php
@@ -21,3 +21,20 @@
 }
 
 add_action('after_setup_theme', 'theme_encrage_init');
+
+if (!function_exists('load_assets')) {
+
+    /**
+     * Chargement des scripts et des styles
+     *
+     * @return void
+     */
+    function load_assets()
+    {
+        wp_enqueue_style('styleCss', get_theme_file_uri('style.css'));
+        wp_enqueue_script('JS', get_theme_file_uri('index.js'), [], null, true);
+        wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+    }
+}
+
+add_action('wp_enqueue_scripts', 'load_assets');
--- website/wp-content/themes/encrage/index.php
+++ website/wp-content/themes/encrage/index.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ENCRAGE - Actualités, reportages et documentaires de 16 photographes.</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+get_header();
+?>
+<?php get_footer(); ?>
