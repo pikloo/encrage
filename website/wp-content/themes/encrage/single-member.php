872de3b77c0e84e74e833d438d76463abee1abb3
pikloo <p.loukakou@gmail.com>
2024-04-28 14:40:19 +0200

    ''

--- website/wp-content/themes/encrage/single-member.php
+++ website/wp-content/themes/encrage/single-member.php
@@ -3,14 +3,9 @@ get_header();
 get_template_part('partials/header', 'header');
 ?>
 <main class="overflow-hidden pt-36">
-    <?php while (have_posts()) : the_post(); ?>
-        <div>
-            <span class="-translate-y-6 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-black before:shadow-lg before:shadow-black/50 relative inline-block">
-                <h1 class="relative text-xl font-medium uppercase text-white"><?php the_title(); ?></h1>
-            </span>
-            <h2>Les photographes</h2>
-        </div>
-    <?php endwhile; ?>
+    <?php get_template_part('partials/member-informations', 'member-informations'); ?>
+    <?php get_template_part('partials/series-list', 'series-list'); ?>
+    <?php get_template_part('partials/releases-list', 'releases-list'); ?>
 </main>
 <?php
 get_template_part('partials/footer', 'footer');
