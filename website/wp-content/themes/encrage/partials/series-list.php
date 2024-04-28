d1852ec2e60ef22c0fcf0b4960ef6ab413b72d7e
pikloo <p.loukakou@gmail.com>
2024-04-29 00:23:22 +0200

    'affichage des séries lié à un photographe'

--- website/wp-content/themes/encrage/partials/series-list.php
+++ website/wp-content/themes/encrage/partials/series-list.php
@@ -1,19 +1,23 @@
 <?php
 $is_home_page = is_front_page() && is_home();
+$is_member_page = 'member' == get_post_type();
+$memberID = $is_member_page ? get_the_ID() :  null;
+
 $args =  [
     'post_type' => 'serie',
     'orderby' => 'year',
     'posts_per_page' => 6,
     'post_status' => 'publish',
     'order' => 'DESC',
-    'meta_key' => 'year',
-    'meta_query' => [
-        [
-            'key' => 'year',
-            'type' => 'CHAR',
-        ]
-    ]
 ];
+
+if ($memberID) {
+    $args['meta_query'][] = [
+        'key' => 'photographer',
+        'value' => $memberID,
+        'compare' => '='
+    ];
+}
 $series = new WP_Query($args);
 ?>
 
@@ -25,14 +29,16 @@ $series = new WP_Query($args);
                 <img class="w-full h-72  object-cover" src="<?= esc_url(get_the_post_thumbnail_url(get_the_ID(), 'thumbnail')) ?>" alt="<?= the_title(); ?>" />
                 <figcaption class="xl:hidden xl:absolute xl:group-hover:block xl:bottom-0 xl:text-white xl:group-hover:animate-slideInUp">
                     <div class="xl:bg-black xl:px-4 xl:text-xl xl:w-fit">
-                        <h3 class="mt-4">
-                            <span class="after:h-[1px] after:bg-black xl:after:bg-white
-              after:inline-block after:relative after:w-[20px] after:align-middle after:ml-1"><?= the_title(); ?></span>
+                        <h3 class="mt-4 <?php if(is_singular()) echo 'inline-block' ?>">
+                            <span class="<?php if(!is_singular()) echo 'after:h-[1px] after:bg-black xl:after:bg-white
+              after:inline-block after:relative after:w-[20px] after:align-middle after:ml-1' ?>"><?= the_title(); ?></span>
+                            <?php if(!is_singular()) :  ?>
                             <span class="ml-2 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-black xl:before:bg-white relative inline-block">
                                 <span class="relative text-white xl:text-black"><?= get_the_title(get_post_meta(get_the_ID(), 'photographer', true)) ?></span>
                             </span>
+                            <?php endif; ?>
                         </h3>
-                        <span class="text-gray-500 lg:text-white font-light"><?= esc_attr(get_post_meta(get_the_ID(), 'year', true)); ?></span>
+                        <p class="text-gray-500 lg:text-white font-light"><?= esc_attr(get_post_meta(get_the_ID(), 'year', true)); ?></p>
                     </div>
                 </figcaption>
             </figure>
@@ -45,6 +51,8 @@ $series = new WP_Query($args);
                 <path id="arrow_move_up" d="m3 2v1h17v-1zm12.5 5.40000153v1.09999847l-3.5015625-2.5-.0000001 14h-.9984374v-14l-3.5 2.5v-1.09999847l4-2.90000153z" fill="currentColor"></path>
             </g>
         </svg>
+        <?php if($series->post_count < $series->found_posts): ?>
         <button class="button" type="button">plus de séries</button>
+        <?php endif; ?>
     </div>
 </section>
--- website/wp-content/themes/encrage/single-member.php
+++ website/wp-content/themes/encrage/single-member.php
@@ -1,11 +1,16 @@
 <?php
 get_header();
 get_template_part('partials/header', 'header');
+$args = array(
+    'post_type' => 'release'
+);
+$releases = new WP_Query( $args );
+$has_releases = $releases->found_posts ?? false;
 ?>
 <main class="overflow-hidden pt-36">
     <?php get_template_part('partials/member-informations', 'member-informations'); ?>
     <?php get_template_part('partials/series-list', 'series-list'); ?>
-    <?php get_template_part('partials/releases-list', 'releases-list'); ?>
+    <?php $has_releases && get_template_part('partials/releases-list', 'releases-list'); ?>
 </main>
 <?php
 get_template_part('partials/footer', 'footer');
