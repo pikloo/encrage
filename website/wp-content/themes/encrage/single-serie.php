0a2cde2bbbf88115c17fa66981949af67ded377b
pikloo <p.loukakou@gmail.com>
2024-05-19 05:10:50 +0200

    ''

--- website/wp-content/themes/encrage/single-serie.php
+++ website/wp-content/themes/encrage/single-serie.php
@@ -26,7 +26,7 @@ $loop = new WP_Query($args);
         <!-- portfolio-title z-[11] md:top-28 text-white before:block before:absolute before:-inset-1 before:skew-y-3 before:bg-black before:shadow-lg before:shadow-black/50 inline-block -->
         <div class="flex flex-col justify-between">
             <section id="serie" class="relative h-[calc(100dvh-5rem)] custom-landscape:h-[calc(100dvh-5rem)] md:h-[calc(100dvh-7rem)] flex flex-col justify-end py-2">
-                <div class="flex flex-col justify-between gap-y-6">
+                <div class="flex flex-col justify-between md:gap-y-6">
                     <div class="portfolio-title fixed bg-white/60 py-3 top-16 custom-landscape:top-16 md:top-28 px-2 md:px-0 w-full justify-center items-center flex z-[11] gap-x-4">
                         <h1 class="p-0 mt-0"><?php the_title(); ?></h1>
                         <div class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-black before:shadow-lg before:shadow-black/50 relative inline-block">
@@ -46,7 +46,49 @@ $loop = new WP_Query($args);
                         <div class="swiper-button-prev text-gray-500"></div>
                     </div>
                     <div class="flex justify-between custom-landscape:justify-between md:justify-center px-4 items-center">
-                        <button onclick="history.go(-1);" class="back-button uppercase font-bold">Retour</button>
+                        <button onclick="history.go(-1);" class="back-button uppercase font-bold">
+                            <svg version="1.1" id="fi_54321" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  width="791.966px" height="791.967px" viewBox="0 0 791.966 791.967" style="enable-background:new 0 0 791.966 791.967;" fill="currentColor" class="w-4 custom-landscape:w-4 md:w-8" xml:space="preserve">
+                                <g>
+                                    <g id="_x37_">
+                                        <g>
+                                            <path d="M245.454,396.017L617.077,56.579c12.973-12.94,12.973-33.934,0-46.874c-12.973-12.94-34.033-12.94-47.006,0
+				L174.615,370.896c-6.932,6.899-9.87,16.076-9.408,25.087c-0.462,9.045,2.476,18.188,9.408,25.088l395.456,361.19
+				c12.973,12.94,34.033,12.94,47.006,0c12.973-12.939,12.973-33.934,0-46.873L245.454,396.017z"></path>
+                                        </g>
+                                    </g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                            </svg>
+                            <span>Retour</span></button>
                         <ul class="flex justify-center gap-x-4 serie-nav">
                             <li>
                                 <a aria-label="Aller à la série photos" href="#serie" aria-current="location" data-serie-anchor-link class="menu-item inline-block w-fit">
